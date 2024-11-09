c64606347f5faff45cacc7d3442e4e995daac91b
Didi <ddgrisalesg@uqvirtual.edu.co>
2024-11-09 09:28:32 -0500

    Lea bien...

--- app/data_analist/request2.py
+++ app/data_analist/request2.py
@@ -13,48 +13,136 @@ df = pd.read_csv(archivo_csv)
 print(df.head())
 
 # Cargar la base de datos
-file_path = 'ruta_del_archivo/bases_unificadas_ok.csv'
+file_path = archivo_csv
 data = pd.read_csv(file_path)
 
 # Definir palabras clave para cada categoría
 categories_keywords = {
-    'Habilidades': ['abstraction', 'algorithm', 'algorithmic thinking', 'coding', 'collaboration', 
-                    'cooperation', 'creativity', 'critical thinking', 'debug', 'decomposition', 
-                    'evaluation', 'generalization', 'logic', 'logical thinking', 'modularity', 
-                    'pattern recognition', 'problem solving', 'programming', 'representation', 
-                    'reuse', 'simulation'],
-    'Conceptos Computacionales': ['conditionals', 'control structures', 'directions', 'events', 
-                                  'functions', 'loops', 'modular structure', 'parallelism', 
-                                  'sequences', 'software', 'hardware', 'variables'],
-    'Actitudes': ['emotional', 'engagement', 'motivation', 'perceptions', 'persistence', 
-                  'self-efficacy', 'self-perceived'],
-    'Propiedades Psicométricas': ['classical test theory', 'confirmatory factor analysis', 
-                                  'exploratory factor analysis', 'item response theory', 
-                                  'reliability', 'structural equation model', 'validity'],
-    'Herramienta de Evaluación': ['bctt', 'escas', 'cctt', 'ctst', 'cta-ces', 'ctc', 'ctls', 
-                                  'cts', 'ctt-es', 'ctt-lp', 'capct', 'ict competency test', 
-                                  'general self-efficacy scale', 'stem-las'],
-    'Diseño de Investigación': ['experimental', 'longitudinal research', 'mixed methods', 
-                                'post-test', 'pre-test', 'quasi-experiments', 'non-experimental'],
-    'Nivel de Escolaridad': ['upper elementary education', 'primary school', 
-                             'early childhood education', 'secondary school', 
-                             'high school', 'university', 'college'],
-    'Medio': ['block programming', 'mobile application', 'pair programming', 'plugged activities', 
-              'programming', 'robotics', 'spreadsheet', 'stem', 'unplugged activities'],
-    'Estrategia': ['construct-by-self mind mapping', 'design-based learning', 'gamification', 
-                   'reverse engineering', 'technology-enhanced learning', 'collaborative learning', 
-                   'cooperative learning', 'flipped classroom', 'game-based learning', 
-                   'inquiry-based learning', 'personalized learning', 'problem-based learning', 
-                   'project-based learning', 'universal design for learning']
+    "Habilidades": [
+        "abstraction",
+        "algorithm",
+        "algorithmic thinking",
+        "coding",
+        "collaboration",
+        "cooperation",
+        "creativity",
+        "critical thinking",
+        "debug",
+        "decomposition",
+        "evaluation",
+        "generalization",
+        "logic",
+        "logical thinking",
+        "modularity",
+        "pattern recognition",
+        "problem solving",
+        "programming",
+        "representation",
+        "reuse",
+        "simulation",
+    ],
+    "Conceptos Computacionales": [
+        "conditionals",
+        "control structures",
+        "directions",
+        "events",
+        "functions",
+        "loops",
+        "modular structure",
+        "parallelism",
+        "sequences",
+        "software",
+        "hardware",
+        "variables",
+    ],
+    "Actitudes": [
+        "emotional",
+        "engagement",
+        "motivation",
+        "perceptions",
+        "persistence",
+        "self-efficacy",
+        "self-perceived",
+    ],
+    "Propiedades Psicométricas": [
+        "classical test theory",
+        "confirmatory factor analysis",
+        "exploratory factor analysis",
+        "item response theory",
+        "reliability",
+        "structural equation model",
+        "validity",
+    ],
+    "Herramienta de Evaluación": [
+        "bctt",
+        "escas",
+        "cctt",
+        "ctst",
+        "cta-ces",
+        "ctc",
+        "ctls",
+        "cts",
+        "ctt-es",
+        "ctt-lp",
+        "capct",
+        "ict competency test",
+        "general self-efficacy scale",
+        "stem-las",
+    ],
+    "Diseño de Investigación": [
+        "experimental",
+        "longitudinal research",
+        "mixed methods",
+        "post-test",
+        "pre-test",
+        "quasi-experiments",
+        "non-experimental",
+    ],
+    "Nivel de Escolaridad": [
+        "upper elementary education",
+        "primary school",
+        "early childhood education",
+        "secondary school",
+        "high school",
+        "university",
+        "college",
+    ],
+    "Medio": [
+        "block programming",
+        "mobile application",
+        "pair programming",
+        "plugged activities",
+        "programming",
+        "robotics",
+        "spreadsheet",
+        "stem",
+        "unplugged activities",
+    ],
+    "Estrategia": [
+        "construct-by-self mind mapping",
+        "design-based learning",
+        "gamification",
+        "reverse engineering",
+        "technology-enhanced learning",
+        "collaborative learning",
+        "cooperative learning",
+        "flipped classroom",
+        "game-based learning",
+        "inquiry-based learning",
+        "personalized learning",
+        "problem-based learning",
+        "project-based learning",
+        "universal design for learning",
+    ],
 }
 
 # Inicializar el conteo de cada categoría
 category_counts = {category: 0 for category in categories_keywords.keys()}
 
 # Procesar cada abstract para clasificarlo en las categorías
-for abstract in data['Abstract'].dropna().str.lower():  # Ignorar valores nulos
+for abstract in data["Abstract"].dropna().str.lower():  # Ignorar valores nulos
     # Dividir en palabras y filtrar por cada categoría
-    words = re.findall(r'\b\w+\b', abstract)  # Lista de palabras individuales
+    words = re.findall(r"\b\w+\b", abstract)  # Lista de palabras individuales
     for category, keywords in categories_keywords.items():
         # Contar cuántas palabras clave de cada categoría aparecen en el abstract
         if any(keyword in words for keyword in keywords):
@@ -65,7 +153,7 @@ print("Conteo de artículos por categoría:", category_counts)
 
 # Generar gráfico de barras
 plt.figure(figsize=(10, 6))
-plt.bar(category_counts.keys(), category_counts.values(), color='skyblue')
+plt.bar(category_counts.keys(), category_counts.values(), color="skyblue")
 plt.xlabel("Categorías")
 plt.ylabel("Número de artículos")
 plt.title("Distribución de artículos por categorías de palabras clave")
